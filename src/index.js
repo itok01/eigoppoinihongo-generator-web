cf47e4e168dac63ae3aaed26a27fa9ba89f49b2c
itok01 <itok01tech@gmail.com>
2019-11-29 20:50:36 +0900

    Create simple UI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apiServer = "https://api.enja.itok01.com";
+const apiSpeech = apiServer + "/speech";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    let speechUrl = apiSpeech + "?text=" + this.state.value;
+    console.log(speechUrl);
+    this.setState({ source: speechUrl })
+    this.refs.speech.pause();
+    this.refs.speech.load();
+    this.refs.speech.play();
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <TextField type="text" value={this.state.value} color="primary" label="日本語" onChange={this.handleChange} />
+          <Button type="submit" className="get-speech-button" variant="contained" color="primary" >
+            変換
+        </Button>
+        </form>
+        <audio controls ref="speech">
+          <source src={this.state.source} type="audio/mp3" />
+        </audio>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Form />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
